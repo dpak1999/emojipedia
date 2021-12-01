599923bce44b18c41bb331f6e50234ea17a97267
dpak1999 <deepakkumardash313@gmail.com>
2021-12-01 23:27:25 +0530

    show all emojis

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 /** @format */
 
+import emojis from './emojis';
+
 function App() {
   return (
     <div className="bg-purple-600 min-h-screen">
       <h1 className="text-white font-bold text-4xl text-center italic font-serif underline">
         Know your emojis
       </h1>
-      <>
-        <div className="flex min-h-screen">
-          <form className="m-auto flex">
-            <input
-              className="rounded-lg p-4 w-96 text-gray-800 outline-none"
-              placeholder="Enter emoji"
-            />
-          </form>
-        </div>
-      </>
+
+      <div className="flex min-h-screen flex-col mt-28">
+        <p className="text-center mb-2">
+          {Object.keys(emojis).map((i) => (
+            <span className="mr-4 text-4xl cursor-default">{i}</span>
+          ))}
+        </p>
+        <form className="flex mr-auto ml-auto">
+          <input
+            className="rounded-lg p-4 w-96 text-gray-800 outline-none"
+            placeholder="Enter emoji"
+          />
+        </form>
+      </div>
     </div>
   );
 }
